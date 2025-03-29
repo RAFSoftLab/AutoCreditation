6de0e87e8e9eb7b452d6a6b5458190c2c2ccf964
stevanzecic <126906285+stevanzecic@users.noreply.github.com>
2025-03-29 01:26:57 +0100

    GUI improvements

--- src/gui/gui_support.py
+++ src/gui/gui_support.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QDialog, QVBoxLayout, QLabel, QPushButton
+from PyQt5.QtWidgets import QDialog, QVBoxLayout, QHBoxLayout, QLabel, QPushButton
 
 class PopupDialog(QDialog):
     def __init__(self, message, title="Warning", parent=None):
@@ -11,8 +11,12 @@ class PopupDialog(QDialog):
 
         label = QLabel(message)
         ok_button = QPushButton("OK")
+        btn_layout = QHBoxLayout()
+        btn_layout.addStretch()
+        btn_layout.addWidget(ok_button)
+        btn_layout.addStretch()
         ok_button.clicked.connect(self.accept)
 
         layout.addWidget(label)
-        layout.addWidget(ok_button)
+        layout.addLayout(btn_layout)
         self.setLayout(layout)
